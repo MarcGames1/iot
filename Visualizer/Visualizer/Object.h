aaf40d8963f317d9de15fa8ef97c22fea537b776
danilanton <katnread@gmail.com>
2023-12-15 16:55:04 +0200

    import texture for models

--- Visualizer/Visualizer/Object.h
+++ Visualizer/Visualizer/Object.h
@@ -4,20 +4,25 @@
 class Object {
 public:
     Object(const std::string& fileName, int x, int y, int width, int height);
+    ~Object() = default;
     void render(const Shader& shader);
     int getX() const;
     int getY() const;
     int getWidth() const;
     int getHeight() const;
     GLuint getTexture() const;
+
+    void GenerateColorTexture(glm::vec3 color, int size);
+    void GenerateTexture(std::string filePath);
+
 protected:
     void load(const std::string& fileName);
-
-    void generateColorTexture(GLuint& texture, glm::vec3 color, int size);
     void renderScene(const Shader& shader);
 
     unsigned int VAO, VBO[3], EBO;
     unsigned int indexCount;
     int x, y, width, height;
     GLuint texture;
+
+    static unsigned int textureCount;
 };
